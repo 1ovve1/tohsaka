58c60d6f1185e5a313b96734fa89bc165c3e8122
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2023-02-23 12:23:16 +0300

    add laradock fix for vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+   /* laradock fix */
+    server: {
+	host: '0.0.0.0',
+	htm: {
+	    host: 'localhost',
+	}
+    },
+
     plugins: [
         vue(),
         laravel({
